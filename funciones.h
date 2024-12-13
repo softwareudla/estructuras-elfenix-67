6b38b5835fa63ae2181a27ef31e6afe16c572aa1
Lucio alumno (thefenix) <111651430+elfenix-67@users.noreply.github.com>
2024-12-13 00:29:27 -0500

    Add files via upload

---
+++ funciones.h
@@ -0,0 +1,21 @@
+﻿#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#define MAX_LIBROS 20
+
+struct Libro {
+    int id;
+    char titulo[101];
+    char autor[51];
+    int anio;
+    char estado[11];
+};
+
+void inicializarBiblioteca(struct Libro biblioteca[], int *contador);
+int agregarLibro(struct Libro biblioteca[], int *contador);
+void mostrarLibros(struct Libro biblioteca[], int contador);
+void buscarLibro(struct Libro biblioteca[], int contador);
+void actualizarEstado(struct Libro biblioteca[], int contador);
+void eliminarLibro(struct Libro biblioteca[], int *contador);
+
+#endif
